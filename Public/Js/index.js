e9e8e342697bcae37fc0c1f30340a7e922f2212a
Toyeeb Ganiu <tboynonilekan@gmail.com>
2020-03-15 20:52:25 +0100

    wrapping with making some images for index  page

--- Public/Js/index.js
+++ Public/Js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function profileDetalis() {
   let userData = JSON.parse(localStorage.getItem("userData"));
   $(".username").text(`Name: ${userData.firstname} ${userData.lastname}`);
   $(".userdes").text(`Description: ${userData.description}`);
-  $(".userLoc").text(`Location: ${userData.state}, ${userData.city}`);
+  $(".userLoc").text(`Location: ${userData.city}, ${userData.state} `);
   $(".userAge").text(`Age: ${userData.age}`);
   //   console.log(userData);
 });
