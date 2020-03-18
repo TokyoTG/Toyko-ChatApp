e5135c884e49c4b930cb7415e43b109ee843b03b
Toyeeb Ganiu <tboynonilekan@gmail.com>
2020-03-18 05:31:25 +0100

    created the edit profile  page about to begin working on it backend

--- Public/Js/index.js
+++ Public/Js/index.js
@@ -1,7 +1,11 @@
 $(document).ready(function profileDetalis() {
   let userData = JSON.parse(localStorage.getItem("userData"));
   $(".username").text(`Name: ${userData.firstname} ${userData.lastname}`);
-  $(".userdes").text(`Description: ${userData.description}`);
+  $(".userdes").text(`Description: ${userData.description}     `);
+  $(".userdes").append(
+    `<i class="fas fa-pencil-alt" data-toggle="tooltip" data-placement="top" title="Edit profile"></i>`
+  );
+  // $('[data-toggle="tooltip"]').tooltip();
   $(".userLoc").text(`Location: ${userData.city}, ${userData.state} `);
   $(".userAge").text(`Age: ${userData.age}`);
   //   console.log(userData);
