d8867fad78b4e683a5518f5c4c8927feac5a3cfa
Toyeeb Ganiu <tboynonilekan@gmail.com>
2020-03-19 01:17:01 +0100

    still putting things together on my edit profile page

--- Public/Js/index.js
+++ Public/Js/index.js
@@ -1,12 +1,25 @@
-$(document).ready(function profileDetalis() {
+$(document).ready(function() {
+  profileDetalis();
+});
+function profileDetalis() {
   let userData = JSON.parse(localStorage.getItem("userData"));
   $(".username").text(`Name: ${userData.firstname} ${userData.lastname}`);
   $(".userdes").text(`Description: ${userData.description}     `);
   $(".userdes").append(
-    `<i class="fas fa-pencil-alt" data-toggle="tooltip" data-placement="top" title="Edit profile"></i>`
+    `<a href="editProfile.html"><i class="fas fa-pencil-alt"ata-toggle="tooltip" data-placement="top" title="Edit profile"></i></a>`
   );
+  $("main").append(
+    '<a href="editProfile.html" class="editP-link" style="display:none">link</a>'
+  );
+
   // $('[data-toggle="tooltip"]').tooltip();
   $(".userLoc").text(`Location: ${userData.city}, ${userData.state} `);
   $(".userAge").text(`Age: ${userData.age}`);
   //   console.log(userData);
-});
+}
+
+// $(".fa-pencil-alt").click(function(e) {
+//   e.preventDefault();
+//   console.log("ok");
+//   $(".editP-link")[0].click();
+// });
---
+++ Public/Js/userIndex.js
@@ -0,0 +1,149 @@
+$(document).ready(function() {
+  getStates();
+});
+
+function getStates() {
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:3000/states",
+    success: function(data) {
+      data.forEach(element => {
+        $("#state").append(`<option value="${element}">${element}</option>`);
+      });
+    }
+  });
+}
+
+$.validator.addMethod(
+  "emailCustom",
+  function(value, element) {
+    return (
+      this.optional(element) || /[a-z0-9.-]+@[a-z-]+\.(com|ng|net)/i.test(value)
+    );
+  },
+  "Please enter a correct email"
+);
+
+$.validator.addMethod("removeSpace", function(value) {
+  return (value = $.trim(value));
+});
+$.validator.addMethod(
+  "phoneNG",
+  function(value, element) {
+    return this.optional(element) || /(081|080|090|070)\d{8}$/.test(value);
+  },
+  "Please enter a correct number"
+);
+
+let selectedState = $("#state");
+$("#state").blur(function() {
+  getCities();
+});
+
+function getCities() {
+  let state = selectedState[0].value;
+  $("#city").html("");
+  if (state !== "") {
+    $.ajax({
+      type: "GET",
+      url: "http://localhost:3000/cities",
+      success: function(data) {
+        data[state].forEach(element => {
+          $("#city").append(`<option>${element}</option>`);
+        });
+      }
+    });
+  }
+}
+
+$("#userReg").validate({
+  rules: {
+    firstname: {
+      required: true,
+      rangelength: [2, 20],
+      lettersonly: true,
+      normalizer: function(value) {
+        return $.trim(value);
+      }
+    },
+    lastname: {
+      required: true,
+      lettersonly: true,
+      rangelength: [2, 20]
+    },
+    phone: {
+      required: true,
+      number: true,
+      phoneNG: true,
+      normalizer: function(value) {
+        return $.trim(value);
+      }
+    },
+    email: {
+      required: true,
+      emailCustom: true,
+      normalizer: function(value) {
+        return $.trim(value);
+      }
+    },
+    age: {
+      required: true,
+      number: true,
+      range: [10, 90],
+      normalizer: function(value) {
+        return $.trim(value);
+      }
+    },
+    gender: {
+      required: true,
+      normalizer: function(value) {
+        return $.trim(value);
+      }
+    },
+    state: {
+      required: true
+    },
+    city: {
+      required: true
+    },
+    description: {
+      required: true,
+      rangelength: [20, 100],
+      normalizer: function(value) {
+        return $.trim(value);
+      }
+    },
+    password: {
+      required: true,
+      minlength: 8,
+      maxlength: 20,
+      nowhitespace: true
+    },
+    password2: {
+      required: true,
+      equalTo: "#password"
+    }
+  },
+  submitHandler: function() {
+    let data = $("#userReg").serializeArray();
+    let res = {};
+    let cap;
+    data.forEach(el => {
+      if (el.name !== "password") {
+        res[el.name] = $.trim(el.value.toLocaleLowerCase());
+      } else {
+        res[el.name] = el.value;
+      }
+    });
+    $.ajax({
+      method: "POST",
+      url: "http://localhost:3000/users",
+      data: res,
+      success: function() {
+        console.log(res);
+        alert("registered");
+      }
+    });
+    return false;
+  }
+});
