457135d52902983756401a4674b0f428ea7f08a9
Toyeeb Ganiu <tboynonilekan@gmail.com>
2020-03-26 21:08:08 +0100

    created the modal for creating chatroom

--- Public/Js/index.js
+++ Public/Js/index.js
@@ -1,6 +1,86 @@
 $(document).ready(function() {
   profileDetalis();
+  popFriend();
 });
+
+function popFriend() {
+  $.ajax({
+    method: "GET",
+    url: `http://localhost:3000/friends?userId=${userDetails.id}`
+  }).done(function(res) {
+    if (res.length) {
+      $(".empty").hide();
+      res.forEach(element => {
+        console.log(element);
+        $("#friendList").append(`<li
+                                    class="list-group-item d-flex justify-content-between align-items-center"
+                                    >
+                                    <a href="#" class="chats" onclick="showProfile(${
+                                      element.friendId
+                                    })"><img
+                                            src="../img/login_icon.png" alt="..."
+                                            class="profile-img">${
+                                              element.name
+                                            }</a>
+
+                                    <a href="#" onclick="addToChats(${
+                                      element.friendId
+                                    })"><span class="badge"
+                                            data-toggle="tooltip" data-placement="top" title="Add to chats"><i
+                                                class="far fa-comment-alt"></i>
+                                        </span></a>
+                                    <a href=""><span class="badge" data-toggle="tooltip" data-placement="top"
+                                            title="Unfriend" onclick="unfriend(${
+                                              element.userId
+                                            },${element.friendId},${
+          element.id
+        },'${element.name.split(" ")[0]}')"> <i
+                                                class="fas fa-trash"></i> </span></a>
+                                </li>`);
+        $("#members").append(
+          `<option id="${element.friendId}">${element.name}</option>`
+        );
+      });
+      $('[data-toggle="tooltip"]').tooltip();
+    }
+  });
+  $.ajax({
+    method: "GET",
+    url: `http://localhost:3000/friends?friendId=${userDetails.id}`
+  }).done(function(res) {
+    if (res.length) {
+      $(".empty").hide();
+      res.forEach(element => {
+        $.ajax({
+          method: "GET",
+          url: `http://localhost:3000/users/${element.userId}`
+        }).done(function(result) {
+          $("#friendList").append(`<li
+                                             class="list-group-item d-flex justify-content-between align-items-center"
+                                             id="${result.id}">
+                                             <a href="#" class="chats" onclick="showProfile(${result.id})"><img
+                                                     src="../img/login_icon.png" alt="..."
+                                                     class="profile-img">${result.firstname} ${result.lastname}</a>
+
+                                             <a href="#" onclick="addToChats(${result.id})"><span
+                                                     class="badge" data-toggle="tooltip"
+                                                     data-placement="top" title="Add to chats"><i
+                                                         class="far fa-comment-alt"></i>
+                                                 </span></a>
+                                             <a href=""><span class="badge" data-toggle="tooltip" data-placement="top"
+                                                     title="Unfriend" onclick="unfriend(${element.id},${element.friendId},${element.id},'${result.firstname}')"> 
+                                                         <i class="fas fa-trash"></i> </span></a>
+                                         </li>`);
+          $("#members").append(
+            `<option id="${result.id}">${result.firstname} ${result.lastname}</option>`
+          );
+          $('[data-toggle="tooltip"]').tooltip();
+        });
+      });
+    }
+  });
+  $('[data-toggle="tooltip"]').tooltip();
+}
 
 function profileDetalis() {
   let userData = JSON.parse(localStorage.getItem("userData"));
@@ -57,7 +137,6 @@ var btn = document.getElementById("menuIcon");
 
 window.onclick = function(event) {
   if (event.target !== modal && event.target !== btn) {
-    console.log("e");
     $("#newSide").css("width", "0");
   }
 };
